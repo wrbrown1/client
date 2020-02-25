f47ee3622a18e5cff13e314fb822dbd65b3cc889
Will <wrbrown@aggies.ncat.edu>
2020-02-25 11:51:35 -0500

    Response functionality

--- src/client/Client.java
+++ src/client/Client.java
@@ -9,19 +9,24 @@ public class Client {
     
     private Socket socket = null;
     private DataInputStream input = null;
+    private DataInputStream inputFromServer = null;
     private DataOutputStream output = null;
     
     public Client(String address, int port) throws IOException{
         
         socket = new Socket(address, port);
         input = new DataInputStream(System.in);
+        inputFromServer = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
         output = new DataOutputStream(socket.getOutputStream());
         
         String userInput = "";
+        String serverResponse = "";
         
         while(!userInput.equals("Disconnect")){
             userInput = input.readLine();
-            output.writeUTF(userInput);         
+            output.writeUTF(userInput);
+            serverResponse = inputFromServer.readUTF();
+            System.out.println(serverResponse);
         }
         
         socket.close();
